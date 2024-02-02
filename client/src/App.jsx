c7747c5a7e63978b39fb427299ef2c6f4b720c5e
Snehajan Jayaram <jaya0024@algonquinlive.com>
2024-02-02 16:03:48 -0500

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Dasboard from './pages/Dasboard'
+import SignIn from './pages/Signin';
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500' > app</h1>
+    <BrowserRouter> <Routes>
+     <Route path="/" element={< Home />} />
+     <Route path="/about" element={<About />} />
+     <Route path="/signin" element={<SignIn /> } />
+     <Route path="/signup" element={<SignUp />} />
+     <Route path="/dashboard" element={<Dasboard />} />
+     <Route path="/projects" element={<Projects />} />
+
+     
+      </Routes>
+      </BrowserRouter>
   )
 }
