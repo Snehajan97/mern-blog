30e28ddc152077440f34e11f7bcbdf0fd36ccf25
Snehajan Jayaram <jaya0024@algonquinlive.com>
2024-02-06 20:02:44 -0500

    created and added the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp'
 import Dasboard from './pages/Dasboard'
 import SignIn from './pages/Signin';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
 
      
       </Routes>
+      <Footer/>
       </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import { Footer } from 'flowbite-react';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs'
+
+export default function FooterCom() {
+  return (
+   <Footer container className='border border-t-8 border-teal-500'>
+    <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+            <div className='mt-5'>
+            <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Snehajan's</span>
+          Blog
+      </Link>
+            </div>
+            <div className='grid grid-col-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6 '>
+             <div>
+            <Footer.Title title='About' />
+            <Footer.LinkGroup col>
+                <Footer.Link href='https://www.100jsprojects.com'
+                             target='_blank'
+                             rel='noopener noreferrer'   >
+                    Sjay JS Projects
+                </Footer.Link>
+                <Footer.Link href='/about'
+                             target='_blank'
+                             rel='noopener noreferrer'   >
+                    Snehajan's Blog
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title title='Follow us' />
+            <Footer.LinkGroup col>
+                <Footer.Link href='https://www.github.com/Snehajan97'
+                             target='_blank'
+                             rel='noopener noreferrer'   >
+                    Github
+                </Footer.Link>
+                <Footer.Link href='#'>
+                    Discord
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title title='Legal' />
+            <Footer.LinkGroup col>
+                <Footer.Link href='#'
+                            >
+                    Privacy Policy
+                </Footer.Link>
+                <Footer.Link href='#'>
+                    Terms & Conditions
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+             </div>
+        </div>
+        <Footer.Divider/>
+        <div className="w-full sm:flex sm:items-center sm:justify-between ">
+            <Footer.Copyright href='#' by="Snehajan's blog" year={new Date().getFullYear()}/>
+            <div className="flex gap-6 sm:mt-4 sm:justify-center">
+                <Footer.Icon href='#' icon={BsFacebook} />
+                <Footer.Icon href='#' icon={BsInstagram} />
+                <Footer.Icon href='#' icon={BsTwitter} />
+                <Footer.Icon href='https://github.com/Snehajan97' icon={BsGithub} />
+                <Footer.Icon href='#' icon={BsDribbble} />
+
+            </div>
+        </div>
+
+    </div>
+    Footer
+   </Footer>
+  )
+}
